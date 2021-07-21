7bfd1536fd9303ce27e9323f4440dbea4b354020
cathepsin <nate.mcmurray13@gmail.com>
2021-07-21 13:34:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from prody import *
 from pylab import *
 
+import sys
+
 ion()
 
-if __name__ == '__main__':
-    p38 = parsePDB('1p38')
-    p38
-    showProtein(p38)
-    input("Press enter to continue...")
+def fStobS(fp):
+    retFP = ""
+    for i in range(len(fp)):
+        if fp[i] == "\\":
+            retFP += "/"
+        else:
+            if (i == 0 or i == len(fp) - 1) and fp[i] == "\"":
+                continue
+            retFP += fp[i]
 
+    return retFP
+
+filepath = input("Input file path to .pdb file ")
+try:
+    f = open(filepath)
+except (FileNotFoundError, OSError):
+    filepath = fStobS(filepath)
+    try:
+        f = open(filepath)
+    except (FileNotFoundError, OSError):
+        sys.stderr.write("File not found. Make sure your file is at the correct location and is a .pdb\nAborted")
+        sys.exit(1) #Abort
+
+
+
+protein = prody.parsePDBStream(f)
+prody.showProtein(protein)
+print("wow")
 

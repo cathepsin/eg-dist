1280ce97e1f2b4a8e7f56ea7aa7bafb83628d274
cathepsin <87338101+cathepsin@users.noreply.github.com>
2021-07-21 16:59:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
-from pymol import stored
+from prody import *
+from pylab import *
+import tkinter as tk
+import Sequence
+from tkinter.filedialog import askopenfilename
+
+import sys
+
+ion()
+
+def fStobS(fp):
+    retFP = ""
+    for i in range(len(fp)):
+        if fp[i] == "\\":
+            retFP += "/"
+        else:
+            if (i == 0 or i == len(fp) - 1) and fp[i] == "\"":
+                continue
+            retFP += fp[i]
+
+    return retFP
+
+def CutPath(str):
+    #Cuts the file path from a file name
+    retStr = str
+    while retStr.find('/') != -1:
+        retStr = retStr[retStr.find('/') + 1:]
+    return retStr
+
+root = tk.Tk()
+root.withdraw()
+
+f_pdb = askopenfilename(title="Select a .pdb file", filetypes=[('Protein Database File', '*.pdb')])
+try:
+    f_p = open(f_pdb)
+except FileNotFoundError:
+    sys.exit("Must select a file")
+
+pmt = "Select associated .socket file for " + CutPath(f_pdb)
+f_sock = askopenfilename(title=pmt, filetypes=[('Socket File', '*.socket')])
+try:
+    f_s = open(f_sock)
+except FileNotFoundError:
+    sys.exit("Must select a file")
+
+protSeq = Sequence.ProteinSequence()
+protSeq.parsePDB(f_p)
+print("wow")
 
 
-def test():
-    print("New command")
 
-
-cmd.extend( "test", test );
